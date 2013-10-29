8b954018dff69d0242a098c9936558dd4ed05ffc
shirailab <shirailab@shirailab-no-MacBook-Air.local>
2013-10-29 12:42:14 +0900

    Initial Commit

---
+++ magic_bleak/HelloWorldLayer.h
@@ -0,0 +1,23 @@
+//
+//  HelloWorldLayer.h
+//  magic_bleak
+//
+//  Created by shirailab on 2013/10/29.
+//  Copyright shirailab 2013年. All rights reserved.
+//
+
+
+#import <GameKit/GameKit.h>
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// HelloWorldLayer
+@interface HelloWorldLayer : CCLayer <GKAchievementViewControllerDelegate, GKLeaderboardViewControllerDelegate>
+{
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
---
+++ magic_bleak/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  magic_bleak
+//
+//  Created by shirailab on 2013/10/29.
+//  Copyright shirailab 2013年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
